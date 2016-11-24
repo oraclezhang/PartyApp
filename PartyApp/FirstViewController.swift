ff560bf82d93aea4e53435745a051d0f8c9809e2
xiangruizhang <xrzhang@gwu.edu>
2016-11-24 16:19:47 -0500

    Initial Commit

---
+++ PartyApp/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  PartyApp
+//
+//  Created by Xiangrui Zhang on 16/11/24.
+//  Copyright © 2016年 Xiangrui Zhang. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ PartyApp/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  PartyApp
+//
+//  Created by Xiangrui Zhang on 16/11/24.
+//  Copyright © 2016年 Xiangrui Zhang. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
